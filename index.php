2299909a47ac23e44c0f94abf221b526dcfc85ce
chasse20 <chasse20@gmail.com>
2017-10-29 23:16:25 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ExampleAPI;
+
+require_once( "includes/interfaces/IAction.php" );
+require_once( "includes/interfaces/IAPI.php" );
+require_once( "includes/interfaces/IConnection.php" );
+require_once( "includes/interfaces/IData.php" );
+require_once( "includes/interfaces/IOutput.php" );
+require_once( "includes/interfaces/IRoute.php" );
+
+require_once( "includes/ActionDelete.php" );
+require_once( "includes/ActionGet.php" );
+require_once( "includes/ActionGetRange.php" );
+require_once( "includes/ActionPost.php" );
+require_once( "includes/ActionPut.php" );
+require_once( "includes/API.php" );
+require_once( "includes/Connection.php" );
+require_once( "includes/DataJSON.php" );
+require_once( "includes/Error.php" );
+require_once( "includes/Output.php" );
+require_once( "includes/Route.php" );
+require_once( "includes/RouteID.php" );
+require_once( "includes/Utility.php" );
+
+// Run API
+$tempAPI = new API( new Connection( "URL", "username", "password", "example_api" ), new Route(), new DataJSON(), new Output() );
+$tempAPI->execute();
+
+?>

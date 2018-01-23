22cb1b66f8354ba025cb981d6a70ae20ca16e3ef
Chris Hassebrook <chasse20@gmail.com>
2018-01-23 14:27:07 -0500

    Update Authorization.php

--- Source/includes/Authorization.php
+++ Source/includes/Authorization.php
@@ -10,10 +10,9 @@ class Authorization implements IAuthorization
 	/**
 	* Validates user authorization
 	* @param IAPI $tAPI API that called this function
-	* @param IConnection $tConnection Reference to Connection instance that will be created
 	* @return bool True if successful
 	*/
-	public function tryAuthorize( IAPI $tAPI, \mysqli $tConnection )
+	public function tryAuthorize( IAPI $tAPI )
 	{
 		return true;
 	}
